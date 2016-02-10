a34cf11fd827db5d4ded5633574f92fb006d28b2
imshashi <shashi.aimit@hotmail.com>
2016-02-10 15:45:55 +0530

    create and view sale page for products with sale price

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, :at => '/'
+
+  Spree::Core::Engine.routes.draw do
+    get "/sale" => "home#sale"
+  end
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
