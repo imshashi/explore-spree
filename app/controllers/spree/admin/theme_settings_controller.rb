e962549cd94b8ddafd3228dec292eb437db964aa
imshashi <shashi.aimit@hotmail.com>
2016-02-13 00:23:04 +0530

    update background's color value after edit

--- app/controllers/spree/admin/theme_settings_controller.rb
+++ app/controllers/spree/admin/theme_settings_controller.rb
@@ -3,9 +3,16 @@ module Spree
     class ThemeSettingsController < Spree::Admin::BaseController
 
       def edit
+        @theme_settings = [ :theme_background_color ]
       end
 
       def update
+        params.each do |name, value|
+          next unless Spree::Config.has_preference? name
+          Spree::Config[name] = value
+        end
+        flash[:success] = Spree.t(:successfully_updated, :resource => Spree.t(:theme_settings))
+
         redirect_to edit_admin_theme_settings_path
       end
     end
